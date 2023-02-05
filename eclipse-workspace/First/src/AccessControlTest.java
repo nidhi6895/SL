30de0f2207b4e39328e1f4f5f694228aa39e0789
Rajput-Nidhi <nidhiarajput6868@gmail.com>
2023-02-05 20:31:03 +0530

    Program on Access control

---
+++ eclipse-workspace/First/src/AccessControlTest.java
@@ -0,0 +1,13 @@
+
+public class AccessControlTest {
+	public static void main(String args[]) {
+		AccessControlDemo obj1= new  AccessControlDemo();
+		obj1.a=10;
+		obj1.b=20;
+		obj1.setC(30);
+		System.out.println();
+				
+		
+	}
+
+}
