8f6d8e343faa2aaa1270cb1b5344a6c5ebf6c5c6
Rajput-Nidhi <nidhiarajput6868@gmail.com>
2023-02-05 20:46:28 +0530

    Area Demo

---
+++ eclipse-workspace/First/src/Area.java
@@ -0,0 +1,11 @@
+
+public class Area {
+
+	public static void main(String args[]) {
+		
+		float radius= 5.6f;
+		final float pie= 3.14f;
+		double area_of_circle= pie * radius * radius;
+		System.out.println("Area of Circle: " + String.format("%.2f", area_of_circle) );
+	}
+}
